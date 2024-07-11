830a738af4e6d49aa4dfac375b2e27dbd186a921
Chris <cgonzalezrodriguez1@csuchico.edu>
2024-07-11 03:25:54 +0000

    actions added

--- ping.js
+++ ping.js
@@ -1,10 +1,24 @@
 let s;
-let xPos = 400;
-let yPos = 200;
+let xPos = innerWidth / 2;
+let yPos = innerHeight / 2;
+let pPos = innerHeight / 2;
+let img;
+let speed = 5;
+let winScreen;
+let side = 7;
+let character;
+
+
+function preload() {
+    img = loadImage('image.png');
+    winScreen = loadImage('youwin.png');
+    character = loadImage('character.gif');
+}
 
 function setup() {
-    createCanvas(screen.width - 30, screen.height -150);
+    createCanvas(innerWidth, innerHeight);
     task(30);
+    
 }
 
 function draw() {
@@ -15,21 +29,51 @@ function draw() {
    
     text(s, width - 65, 45);
     textSize(40);
-
+    image(img, innerWidth * 1 / 4, innerHeight / 4, innerWidth / 2, innerHeight / 2);
     circle(xPos, yPos, 50);
-    if (xPos > width) {
-        xPos -= 5;
+    image(character, innerWidth * 1 / 5 - 100, pPos, 300, 300);
+    playerMove();
+
+if (yPos <= innerHeight && side == 7) {
+    yPos += 2;
+}
+    
+    if ((xPos >= Math.floor((innerWidth * 3 / 4) - 10) && xPos <= Math.floor((innerWidth * 3 / 4) + 10))) {
+       if (yPos == innerHeight / 2) {
+        speed = -speed;
+        }
+        else if (yPos > innerHeight / 2  && yPos <= innerHeight / 2 + 1000) {
+            speed = -speed;
+            side = 9;
+        }
+        else {
+            speed = -speed;
+            side = 9;
+        }
     }
-    else if (xPos < 0) {
-        xPos
+    else if (xPos >= Math.floor((innerWidth * 1 / 4) - 10) && xPos <= Math.floor((innerWidth * 1 / 4) + 10)) {
+        console.log(yPos)
+        speed = -speed;
+        if (keyIsDown(65)) {
+        side = 0;
+        }
+        else if ((keyIsDown(68) && pPos == 162 && yPos == 325)) {
+
+            side = 2;
+        }
+        else {
+            side = 9;
+        }
     }
+    ballPos();
+   xPos += speed;
 
     if (s === 0) {
-        help
-    hi
+        background("black");
+        image(winScreen, 0, 0, innerWidth, innerHeight);
+        return;
     }
-    
-    
+   
 }
 function task(t) {
    if (t >= 0) {
@@ -38,4 +82,34 @@ function task(t) {
         task(t - 1);
     }, 1000)
 }
+}
+
+function ballPos() {
+    if (side === 0) {
+        yPos -= 2;
+    }
+    else if (side === 1) {
+        yPos += 2;
+    }
+    else if (side === 2) {
+        yPos += 2;
+    }
+    else if (side === 3) {
+        yPos += 2;
+ 
+    }
+    else if (side === 9) {
+        yPos += 0;
+    }
+}
+
+function playerMove() {
+   
+    if (keyIsDown(87) && pPos > (innerHeight / 2) - innerWidth / 4 + 100){
+            pPos -= 3.5;
+        }
+    
+    if (keyIsDown(83) && pPos <= ((innerHeight / 2) + innerWidth / 4) - 400) {
+        pPos += 3.5;
+    }
 }
